209e8bfaecbcb3154a8060ec7457d5d828fc2c42
Santiago Emanuel Nieva <59318726+pragmatically-dev@users.noreply.github.com>
2022-03-16 01:43:56 +0100

    fix typo

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use yew::prelude::*;
 
 enum Msg{
     AddOne,
-    Substract,
+    Subtract,
 }
 
 
@@ -28,7 +28,7 @@ impl Component for Model{
                 self.value +=1;
                 true
             },
-            Msg::Substract=>{
+            Msg::Subtract=>{
                 self.value-=1;
                 true
             }
@@ -40,7 +40,7 @@ impl Component for Model{
         html!{
             <div class={"top-20 bg-white w-69 h-56 rounded-lg"}>
             <button class={"bg-slate-600 p-4 m-6 rounded-lg text-white hover:bg-slate-800 duration-500"} onclick={link.callback(|_|Msg::AddOne)} >{"Add One"}</button>
-            <button class={"bg-slate-600 p-4 m-6 rounded-lg text-white hover:bg-slate-800 duration-500"} onclick={link.callback(|_|Msg::Substract)} >{"Substrack one"}</button>
+            <button class={"bg-slate-600 p-4 m-6 rounded-lg text-white hover:bg-slate-800 duration-500"} onclick={link.callback(|_|Msg::Subtract)} >{"Substrack one"}</button>
             <p class={"flex justify-center mt-10 font-semibold text-3xl bg-slat-700"}>{self.value}</p>
             </div>
         }
